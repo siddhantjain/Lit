5fe5635f260515fbe1af363f270bab5cbdcf2f3f
akashjatangi <akashjatangi@gmail.com>
2014-03-22 11:52:33 +0530

    file input added

--- src/main.py
+++ src/main.py
@@ -6,14 +6,16 @@ if __name__ == '__main__':
    
 
     lx = lexicalanalyser.Lexer(regexes)
-    inputstring = '_functionName int a; $a <- $b+$c'
-    lx.input(inputstring)
+    progfile = open("program_lit.txt","r")
+    errorfile = open("error_lit.txt","w")
+    tokfile = open("tokens_lit.txt","w")
+    lineno = 1
     
-    
-    
-    for tok in lx.tokens():
-        test = tok.type_
-        if tok.type_ == 'TK_ERROR':
-            print ('PRINT IN ERROR FILE:    %s'%tok.__str__())        
-        else:
-            print(tok)
+    for line in progfile:
+    	lx.input(line)
+    	for tok in lx.tokens():
+    	   if tok.type_ == 'TK_ERROR':
+    	        errorfile.write ('(%d,%d) \'%s\' doesn\'t follow lexical rules'% (tok.pos,lineno,tok.val))        
+    	   else:
+    	        tokfile.write ('%s '%tok.type_)
+    	lineno+=1

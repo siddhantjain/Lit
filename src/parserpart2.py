1b9e121f66f88885d9797c02778ebcf4465363ed
akashjatangi <akashjatangi@gmail.com>
2014-03-23 22:54:13 +0530

    Rules Dictionary created

--- src/parserpart2.py
+++ src/parserpart2.py
@@ -1,15 +1,13 @@
 import csv
-import collections
+from collections import defaultdict
 
 if __name__ == '__main__':
     
     #progrules = open("examples/programrules.txt","r")
     ruleslistfile = open("src/Grammar_23Mar_Full.csv","r")
-    
     RulesDict = defaultdict(list)
-    reader = csv.DictReader(ruleslistfile) #create a reader which represents rows in a dictionary form
+    reader = csv.DictReader(ruleslistfile,['Rule Number','LHS','RHS']) #create a reader which represents rows in a dictionary form
     for row in reader: #this will read a row as {column1: value1, column2: value2,...}
-        for (k,v) in row.items(): #go over each column name and value 
-            RulesDict[k].append(v) #append the value into the appropriate list based on column name k
+        RulesDict[row['Rule Number']].append('%s42%s'%(row['LHS'],row['RHS']))
     
-    print(RulesDict['Rule Number'])
+    print(RulesDict['RULE1'])

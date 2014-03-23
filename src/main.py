52a6581eff003145b6c946e594e7f9ab4bf49ac9
Bluenotebook <post.siddhant@gmail.com>
2014-03-23 10:54:50 +0530

    bug with commentsremoved

--- src/main.py
+++ src/main.py
@@ -6,16 +6,22 @@ if __name__ == '__main__':
    
 
     lx = lexicalanalyser.Lexer(regexes)
-    progfile = open("program_lit.txt","r")
+    progfile = open("examples/test1.txt","r")
     errorfile = open("error_lit.txt","w")
     tokfile = open("tokens_lit.txt","w")
     lineno = 1
-    
+    counttok = 0
     for line in progfile:
     	lx.input(line)
-    	for tok in lx.tokens():
+        alltokens = lx.tokens()
+        
+    	for tok in alltokens:
     	   if tok.type_ == 'TK_ERROR':
-    	        errorfile.write ('(%d,%d) \'%s\' doesn\'t follow lexical rules'% (tok.pos,lineno,tok.val))        
+    	        errorfile.write ('(%d,%d) \'%s\' doesn\'t follow lexical rules'% (lineno,tok.pos,tok.val))
+           if tok.type_ == 'TK_CMNT':
+                continue        
     	   else:
     	        tokfile.write ('%s '%tok.type_)
+           
     	lineno+=1
+        

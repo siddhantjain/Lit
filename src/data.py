497f1d7014476c4127b4e6b0ff46e57f04a3e160
akashjatangi <akashjatangi@gmail.com>
2014-03-25 01:44:14 +0530

    Parser Implemented in 2 parts

--- src/data.py
+++ src/data.py
@@ -13,10 +13,11 @@ regexes = [
 (r'\]','TK_CSQ'),
 (r';','TK_SCLN'),
 ('if','TK_IF'),
-('endif','TK_EIF'),			#Placed before end to satisfy principal of longest match
+('endif','TK_ENDIF'),			#Placed before end to satisfy principal of longest match
 ('else','TK_ELSE'),
 ('return','TK_RET'),
-('end','TK_FEND'),
+('endwhile','TK_EWHILE'),       #Placed before end to satisfy principal of longest match
+('end','TK_END'),
 ('int','TK_INT'),
 ('float','TK_FLOAT'),
 ('read','TK_READ'),
@@ -26,7 +27,6 @@ regexes = [
 (r'\(','TK_ORD'),
 (r'\)','TK_CRD'),
 ('while','TK_WHILE'),
-('endwhile','TK_EWHILE'),
 (r'<\-','TK_ASSIGN'),		
 ('==','TK_EQ'),
 ('<=','TK_LTE'),		    #NOTE: Ordering is important for principal of longest match
@@ -46,3 +46,66 @@ regexes = [
 ('void','TK_VOID')
 
     ]
+
+nonTerminals = [
+'<Program>',
+'<O_functions>',
+'<main_function>',
+'<stmts>',
+'<O_stmts>',
+'<stmt>',
+'<gen_stmt>',
+'<print_stmts>',
+'<print_stmt>',
+'<println_stmt>',
+'<all_var>',
+'<id>',
+'<id_1>',
+'<id_2>',
+'<read_stmt>',
+'<declrtv_stmts>',
+'<declrtv_stmt>',
+'<d_s>',
+'<global_stmt>',
+'<type>',
+'<arithmetic_expression>',
+'<a_e>',
+'<term>',
+'<t_e>',
+'<factor>',
+'<var>',
+'<Boolean_expression>',
+'<b_e>',
+'<logical_operator>',
+'<relational_operator>',
+'<cond_stmt>',
+'<c_s>',
+'<iterative_stmt>',
+'<funcall_Stmt>',
+'<list_var>',
+'<next>',
+'<break_stmt>',
+'<assignm_stmt>',
+'<function>',
+'<input_parameters>',
+'<i_p>',
+'<output_parameters>',
+'<o_p>',
+'<parameter_list>',
+'<nextpl>',
+'<return_stmt>',
+'<r_s>'
+]
+
+terminals = [
+'TK_ASSIGN',	'TK_ID',	'TK_NUM', 'TK_RNUM',	'TK_IPP',	
+'TK_OPP',	'TK_FUNC',	'TK_OSQ', 'TK_CSQ',	'TK_SCLN',	
+'TK_RET',	'TK_END',	'TK_MAIN',	'TK_INT',	'TK_FLOAT',	
+'TK_READ',	'TK_CALL',	'TK_PRINT',	'TK_PRINTLN',	'TK_ORD',	
+'TK_CRD',	'TK_WHILE',	'TK_EWHILE',	'TK_IF',	'TK_ENDIF',	
+'TK_ELSE',	'TK_EQ',	'TK_LTE',	'TK_LESS',	'TK_GTE',	
+'TK_GRTR',	'TK_NOT',	'TK_NOTEQ',	'TK_AND',	'TK_OR',	
+'TK_PLUS',	'TK_MINUS',	'TK_MUL',	'TK_DIV',	'TK_BREAK',	
+'TK_COMMA',	'TK_GLOBAL',	'TK_VOID',	'$',	'TK_STRLIT'
+]
+

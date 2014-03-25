28bdf87dd30ba924ea2e6afe4e27bca5438d50a9
Bluenotebook <post.siddhant@gmail.com>
2014-03-25 11:10:14 +0530

    Updating error handling mechanism

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import lexicalanalyser
 import parser_pda
+import parserpart2
+
 from data import regexes
 
 #weird way, but this is how main can start
@@ -8,7 +10,7 @@ if __name__ == '__main__':
 
     lx = lexicalanalyser.Lexer(regexes)
 
-    progfile    =  open("examples/test1.txt","r")       #TODO: Change all these to command line arguments
+    progfile    =  open("examples/test2.txt","r")       #TODO: Change all these to command line arguments
     errorfile   =  open("error_lit.txt","w")
     tokfile     =  open("tokens_lit.txt","w")
     
@@ -19,7 +21,7 @@ if __name__ == '__main__':
     	for tok in alltokens:
     	   if tok.type_ == 'TK_ERROR':
     	        errorfile.write ('(%d,%d) \'%s\' doesn\'t follow lexical rules'% (lineno,tok.pos,tok.val))
-           if tok.type_ == 'TK_CMNT':
+           elif tok.type_ == 'TK_CMNT':
                 continue        
     	   else:
     	        tokfile.write ('%s '%tok.type_)
@@ -38,5 +40,14 @@ if __name__ == '__main__':
         
     rulegenerator = parser_pda.PushDownAutomata(parsetable,grammar,listofTokens)
     generatedrules = rulegenerator.PDAOperation()
-
-    print(generatedrules)
+    
+    if generatedrules[-2] == 'Syntax Error':
+        errorfile.write(generatedrules[-1]) 
+         
+    #Creating the parse Tree
+    else:
+        RulesDict = parserpart2.createRulesDict(grammar)
+        nextrule = parserpart2.RuleNumber(0)
+        Head = parserpart2.BuildTree(generatedrules,nextrule,RulesDict)
+       
+    #parserpart2.printtree(Head)
--- src/parserpart2.py
+++ src/parserpart2.py
@@ -9,84 +9,67 @@ class Node(object):
 	
 
 	
-	def add_children(self,obj):
-		self.children.append(obj)
+    def add_children(self,obj):
+        self.children.append(obj)
 
 class RuleNumber(object):
-	def __init__(self,val):
-		self.val = 0
+    def __init__(self,val):
+        self.val = val
 
 
 def printtree(obj):
-	print("parent is %s and children are: " % obj.val)
-	if obj.children:
-		for child in obj.children:
-			print(" %s " %child.val)
-		#print("\n")
-		for child in obj.children:
-			if child:
-				printtree(child)
-	else:
-		print("None")
+    print("parent is %s and children are: " % obj.val)
+    if obj.children:
+        for child in obj.children:
+            print(" %s " %child.val)
+        #print("\n")
+        for child in obj.children:
+            if child:
+                printtree(child)
+    else:
+        print("None")
 
-def BuildTree(alllines,nextrule):
-	
+def BuildTree(alllines,nextrule,RulesDict):
 	#line = progrulefile.next()
-	line = alllines[nextrule.val]
-	nextrule.val+=1
-	unit = re.search(r"\s",line)
-	#print("flag 1 %s" %line[0:unit.start()])
-	ruleno = line[0:unit.start()]
-	nonterm = RulesDict[ruleno][0]
-	newnode = Node(nonterm)
-	RHS = RulesDict[ruleno][1]
-	
-	for eachsymbol in RHS.split():
-		#print("each symbol %s"%eachsymbol)
-		if(re.match("TK_.*",eachsymbol)):				#the symbol is a terminal
-			newnode2 = Node(eachsymbol)
-			newnode2.children = None
-			newnode.children.append(newnode2)
+    ruleno = alllines[nextrule.val]
+    nextrule.val += 1
+    nonterm = RulesDict[ruleno][0]
+    newnode = Node(nonterm)
+    RHS = RulesDict[ruleno][1]
+    
+    for eachsymbol in RHS.split():
+        #print("each symbol %s"%eachsymbol)
+        if(re.match("TK_.*",eachsymbol)):				#the symbol is a terminal
+            newnode2 = Node(eachsymbol)
+            newnode2.children = None
+            newnode.children.append(newnode2)
 			
-		elif(re.match("<.*>",eachsymbol)):				#symbol is a non-terminal
-			#progrulefile2 = progrulefile
-			nextrule2 = nextrule
-			#nextline = progrulefile2.next()
-			nextline = alllines[nextrule2.val]
-			nextunit = re.search(r"\s",nextline)
-			#print(nextline[0:unit.start()])
-			nextruleno = nextline[0:nextunit.start()]
-			nextnonterm = RulesDict[nextruleno][0]
-			matchedsymrule = re.match(nextnonterm,eachsymbol)
-			if matchedsymrule:
-				newnode3 = BuildTree(alllines,nextrule)
-				newnode.children.append(newnode3)
+        elif(re.match("<.*>",eachsymbol)):				#symbol is a non-terminal
+            #progrulefile2 = progrulefile
+            nextrule2 = nextrule
+            #nextline = progrulefile2.next()
+            nextruleno = alllines[nextrule2.val]
+            nextnonterm = RulesDict[nextruleno][0]
+            matchedsymrule = re.match(nextnonterm,eachsymbol)
+            if matchedsymrule:
+                newnode3 = BuildTree(alllines,nextrule,RulesDict)
+                newnode.children.append(newnode3)
 	
-	return newnode
+    return newnode
 
 
-
-if __name__ == '__main__':
-    
-    #progrules = open("examples/programrules.txt","r")
-    ruleslistfile = open("Grammar.csv","r")
+def createRulesDict(Grammar):
+    ruleslistfile = open(Grammar,"r")
     RulesDict = defaultdict(list)
-    reader = csv.DictReader(ruleslistfile,['Rule Number','LHS','RHS'],delimiter='\t') #create a reader which represents rows in a dictionary form
+    reader = csv.DictReader(ruleslistfile,['Rule','LHS','RHS'],delimiter=',') #create a reader which represents rows in a dictionary form
     for row in reader: #this will read a row as {column1: value1, column2: value2,...}
-        #RulesDict[row['Rule Number']].append('%s42%s'%(row['LHS'],row['RHS']))
-        RulesDict[row['Rule Number']].append(row['LHS'])
-        RulesDict[row['Rule Number']].append(row['RHS'])
+        RulesDict[row['Rule']].append(row['LHS'])
+        RulesDict[row['Rule']].append(row['RHS'])
+    return RulesDict
+
+
     
-	#print(RulesDict)
-    #parsetreegen("examples/test2rules.txt")
-    
-    progrulefile = open("examples/test2rules.txt")
-    alllines = progrulefile.readlines()
-    nextrule = RuleNumber(0)
-    Head = BuildTree(alllines,nextrule)
-    #Head = BuildTree(progrulefile)
-    
-    printtree(Head)
+
     
 
 

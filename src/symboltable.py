0ed4fb27a5b3dd7afcc7e488268f0534152daacd
Siddhant Jain <post.siddhant@gmail.com>
2014-04-10 16:42:32 +0530

    Delete symboltable.py

--- src/symboltable.py
+++
@@ -1,8 +0,0 @@
-class symbolTable:
-    def __init__(self,name,token,lineoccur):        #TODO:Extend definition of Symbol Table
-        self.name = name
-        self.token = token
-        self.lineoccur = lineoccur
-    def __str__(self):
-        return "Name = %s | Token = %s | Line of occurence = %d "%(self.name,self.token,self.lineoccur)
-
